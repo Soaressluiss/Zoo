f92576013b4582f6936bcc75bcb66ac884256832
Luis Soares <luissoaress.dev@gmail.com>
2022-10-23 23:58:50 -0300

    update carrosels

---
+++ assets/scripts/carroselComentarios.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    $('.carrosel-comentarios').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        arrows:true,
+    });
+});
