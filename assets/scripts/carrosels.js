f6846f833588b3975a31db29826dba09be0eac7e
Luis Soares <luissoaress.dev@gmail.com>
2022-10-24 23:44:39 -0300

    create footer

---
+++ assets/scripts/carrosels.js
@@ -0,0 +1,31 @@
+
+const prev = document.querySelector(".fa-circle-arrow-left")
+const next = document.querySelector(".fa-circle-arrow-right")
+
+
+$(document).ready(function () {
+    $('.carrosel-swiper').slick({
+        infinite: true,
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        arrows:false,
+        // prevArrow: prev,
+        // nextArrow: next,
+
+    });
+});
+$(document).ready(function () {
+    $('.carrosel-comentarios').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        arrows:true,
+        prevArrow: prev,
+        nextArrow: next,
+
+    });
+});
